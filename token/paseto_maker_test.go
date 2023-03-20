1adee75132346640bc6dad0bf9bb47a99e7bf893
Bagus Oktaviadi <bagusoktaviadi1@gmail.com>
2023-03-20 20:22:49 +0700

    Jwt Remove and middleware test init

--- token/paseto_maker_test.go
+++ token/paseto_maker_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestPasetoMaker(t *testing.T) {
-	maker, err := NewJWTMaker(util.RandomString(32))
+	maker, err := NewPasetoMaker(util.RandomString(32))
 	require.NoError(t, err)
 
 	username := util.RandomOwner()
